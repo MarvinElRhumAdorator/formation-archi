fb0e51037d7f7f97091a0deba2ebfc1fb6803ccc
MarvinElRhumAdorator <marvingilly@yahoo.fr>
2023-09-08 02:03:34 +0200

    remove persistence from domain entity

--- domain/src/main/java/com/example/domain/model/Product.java
+++ domain/src/main/java/com/example/domain/model/Product.java
@@ -1,27 +1,20 @@
 package com.example.domain.model;
 
-import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Objects;
 
-@Entity
-@Table(name = "product")
 @Getter
 @Setter
 @ToString
-@RequiredArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of = {"code"})
 public class Product {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     private String name;
     private String description;
 
-    @Column(unique=true)
     private long code;
     private long createdAt;
 
@@ -43,16 +36,4 @@ public class Product {
                 this.getCreatedAt());
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product that = (Product) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
